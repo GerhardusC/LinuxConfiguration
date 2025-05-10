eb46f48290cfd68fa3e5325fd44942b7dfcabed6
Gerhardus <133974851+GerhardusC@users.noreply.github.com>
2025-05-10 09:50:29 +0200

    fix: fixed docker install

--- init.sh
+++ init.sh
@@ -56,8 +56,6 @@ newgrp dialout
 
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
-Executing docker install script, commit: 7cae5f8b0decc17d6571f9f52eb840fbc13b2737
-<...>
 
 sudo usermod -aG docker $USER
 newgrp docker
