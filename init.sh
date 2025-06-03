f562e5030ce83e25d58b0020b77c77d7ca8d7f8e
Gerhardus <hardus.cronje@gmail.com>
2025-06-03 18:04:08 +0200

    fix: added alacritty config

--- init.sh
+++ init.sh
@@ -9,13 +9,17 @@ sudo apt install -y tree tmux vim;
 # Extra project stuff
 sudo apt install -y mosquitto mosquitto-clients;
 
+# Copy all config files.
 cp ./.bashrc ~/.bashrc;
 cp ./.tmux.conf ~/.tmux.conf;
+mkdir $HOME/.config/alacritty;
+cp ./alacritty.toml $HOME/.config/alacritty;
 
+# Install rust toolchain
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh;
-
 . "$HOME/.cargo/env";
 
+# Install node toolchain
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
@@ -28,18 +32,27 @@ node -v;
 
 npm -v;
 
-git clone https://github.com/GerhardusC/neovim-config $HOME/.config/nvim
-
-sudo apt-get install -y git wget flex bison gperf python3 python3-pip python3-venv cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0
-
+# Install neovim.
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 
+# Configure neovim.
 git clone https://github.com/GerhardusC/neovim-config $HOME/.config/nvim
 
+# Set keyboard repeat rate.
 gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 30
 gsettings set org.gnome.desktop.peripherals.keyboard delay 250
+
+# Install Docker.
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+sudo usermod -aG docker $USER
+newgrp docker
+
+# ESP 32 stuff. If it fails its whatever.
+sudo apt-get install -y git wget flex bison gperf python3 python3-pip python3-venv cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0
 
 mkdir -p ~/esp
 cd ~/esp
@@ -50,14 +63,9 @@ cd ~/esp/esp-idf
 
 . $HOME/esp/esp-idf/export.sh
 
+# Add to group to be able to use USB.
 sudo usermod -aG dialout $USER
 newgrp dialout
 
-
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-
-sudo usermod -aG docker $USER
-newgrp docker
 
 
